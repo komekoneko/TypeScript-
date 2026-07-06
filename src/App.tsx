f661aaf50531e2d7f6811d0dd560e6435bb71a43
komekoneko <kai.s20050701@gmail.com>
2026-07-06 21:54:59 +0900

    add counter function

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
+
+  const countUp = () => {
+    setCount((prev) => prev + 1)
+  }
+
+  const countDown = () => {
+    setCount((prev) => prev - 1)
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
 
   return (
     <>
       <h1>カウンターApp(TypeScriptバージョン)</h1>
+      <h2>現在のカウント: {count}</h2>
+      <button onClick={countUp}>+1をする</button>
+      <button onClick={countDown}>-1をする</button>
+      <button onClick={reset}>リセットをする</button>
     </>
   )
 }
